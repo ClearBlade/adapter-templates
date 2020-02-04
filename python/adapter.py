cec66231cada781b0fa4fa696eb4d194f31e6d1e
Michael Sprague <msprague@clearblade.com>
2020-02-04 13:47:34 -0700

    added support for adapter environment variables and device service accounts

--- python/adapter.py
+++ python/adapter.py
@@ -11,92 +11,118 @@ from clearblade.ClearBladeCore import cbLogs
 ADAPTER_NAME = "MyAdapter"
 CB_CONFIG = {}
 
+
+def parse_env_variables(env):
+    """Parse environment variables"""
+    possible_vars = ["CB_SYSTEM_KEY", "CB_SYSTEM_SECRET", "CB_EDGE_NAME", "CB_PLATFORM_IP", "CB_EDGE_IP", "CB_ADAPTERS_ROOT_DIR", "CB_SERVICE_ACCOUNT", "CB_SERVICE_ACCOUNT_TOKEN"]
+    
+    for var in possible_vars:
+        if var in env:
+            print("Setting config from environment variable: " + var)
+            CB_CONFIG[var] = env[var]
+
+    #TODO Add implementation specific environment variables here
+
+
 def parse_args(argv):
     """Parse the command line arguments"""
 
-    parser = argparse.ArgumentParser(description='Start ClearBlade Adapter')
-    parser.add_argument('--systemKey', required=True, help='The System Key of the ClearBlade \
+    parser = argparse.ArgumentParser(description='ClearBlade Adapter')
+    parser.add_argument('-systemKey', dest="CB_SYSTEM_KEY", help='The System Key of the ClearBlade \
                         Plaform "System" the adapter will connect to.')
 
-    parser.add_argument('--systemSecret', required=True, help='The System Secret of the \
+    parser.add_argument('-systemSecret', dest="CB_SYSTEM_SECRET", help='The System Secret of the \
                         ClearBlade Plaform "System" the adapter will connect to.')
 
-    parser.add_argument('--deviceID', required=True, \
-                        help='The id/name of the device that will be used for device \
+    parser.add_argument('-deviceID', dest="deviceID", help='The id/name of the device that will be used for device \
                         authentication against the ClearBlade Platform or Edge, defined \
                         within the devices table of the ClearBlade platform.')
 
-    parser.add_argument('--activeKey', required=True, \
-                        help='The active key of the device that will be used for device \
+    parser.add_argument('-activeKey', dest="activeKey", help='The active key of the device that will be used for device \
                         authentication against the ClearBlade Platform or Edge, defined within \
                         the devices table of the ClearBlade platform.')
 
-    parser.add_argument('--httpUrl', dest="httpURL", default="http://localhost", \
+    parser.add_argument('-cb_service_account', dest="CB_SERVICE_ACCOUNT", help='The id/name of the device service accountthat will be used for \
+                        authentication against the ClearBlade Platform or Edge, defined \
+                        within the devices table of the ClearBlade platform.')
+
+    parser.add_argument('-cb_service_account_token', dest="CB_SERVICE_ACCOUNT_TOKEN", help='The token of the device service account that will be used for device \
+                        authentication against the ClearBlade Platform or Edge, defined within \
+                        the devices table of the ClearBlade platform.')
+
+    parser.add_argument('-httpUrl', dest="httpURL", default="http://localhost", \
                         help='The HTTP URL of the ClearBlade Platform or Edge the adapter will \
                         connect to. The default is https://localhost.')
 
-    parser.add_argument('--httpPort', dest="httpPort", default="9000", \
+    parser.add_argument('-httpPort', dest="httpPort", default="9000", \
                         help='The HTTP Port of the ClearBlade Platform or Edge the adapter will \
                         connect to. The default is 9000.')
 
-    parser.add_argument('--messagingUrl', dest="messagingURL", default="localhost", \
+    parser.add_argument('-messagingUrl', dest="messagingURL", default="localhost", \
                         help='The MQTT URL of the ClearBlade Platform or Edge the adapter will \
                         connect to. The default is https://localhost.')
 
-    parser.add_argument('--messagingPort', dest="messagingPort", default="1883", \
+    parser.add_argument('-messagingPort', dest="messagingPort", default="1883", \
                         help='The MQTT Port of the ClearBlade Platform or Edge the adapter will \
                         connect to. The default is 9000.')
 
-    parser.add_argument('--adapterSettingsCollection', dest="adapterSettingsCollectionName", \
+    parser.add_argument('-adapterSettingsCollection', dest="adapterSettingsCollectionName", \
                         default="", \
                         help='The name of the ClearBlade Platform data collection which contains \
                         runtime configuration settings for the adapter. The default is "".')
 
-    parser.add_argument('--adapterSettingsItem', dest="adapterSettingsItemID", default="", \
+    parser.add_argument('-adapterSettingsItem', dest="adapterSettingsItemID", default="", \
                         help='The "item_id" of the row, within the ClearBlade Platform data \
                         collection which contains runtime configuration settings, that should \
                         be used to configure the adapter. The default is "".')
 
-    parser.add_argument('--topicRoot', dest="adapterTopicRoot", default="", \
+    parser.add_argument('-topicRoot', dest="adapterTopicRoot", default="", \
                         help='The root of MQTT topics this adapter will subscribe and publish to. \
                         The default is "".')
 
-    parser.add_argument('--deviceProvisionSvc', dest="deviceProvisionSvc", default="", \
-                        help='The name of a service that can be invoked to provision IoT devices \
-                        within the ClearBlade Platform or Edge. The default is "".')
-
-    parser.add_argument('--deviceHealthSvc', dest="deviceHealthSvc", default="", \
-                        help='The name of a service that can be invoked to provide the health of \
-                        an IoT device to the ClearBlade Platform or Edge. The default is "".')
-
-    parser.add_argument('--deviceLogsSvc', dest="deviceLogsSvc", default="", \
-                        help='The name of a service that can be invoked to provide IoT device \
-                        logging information to the ClearBlade Platform or Edge. The default is "".')
-
-    parser.add_argument('--deviceStatusSvc', dest="deviceStatusSvc", default="", \
-                        help='The name of a service that can be invoked to provide the status of \
-                        an IoT device to the ClearBlade Platform or Edge. The default is "".')
-
-    parser.add_argument('--deviceDecommissionSvc', dest="deviceDecommissionSvc", default="", \
-                        help='The name of a service that can be invoked to decommission IoT \
-                        devices within the ClearBlade Platform or Edge. The default is "".')
-
-    parser.add_argument('--logLevel', dest="logLevel", default="INFO", choices=['CRITICAL', \
+    parser.add_argument('-logLevel', dest="logLevel", default="INFO", choices=['CRITICAL', \
                         'ERROR', 'WARNING', 'INFO', 'DEBUG'], help='The level of logging that \
                         should be utilized by the adapter. The default is "INFO".')
 
-    parser.add_argument('--logCB', dest="logCB", default=False, action='store_true',\
+    parser.add_argument('-logCB', dest="logCB", default=False, action='store_true',\
                         help='Flag presence indicates logging information should be printed for \
                         ClearBlade libraries.')
 
-    parser.add_argument('--logMQTT', dest="logMQTT", default=False, action='store_true',\
+    parser.add_argument('-logMQTT', dest="logMQTT", default=False, action='store_true',\
                         help='Flag presence indicates MQTT logs should be printed.')
 
     #TODO Add implementation specific command line arguments here
 
+    args = vars(parser.parse_args(args=argv[1:]))
+    for var in args:
+        if args[var] != "" and args[var] != None:
+            print("Setting config from command line argument: " + var)
+            CB_CONFIG[var] = args[var]
 
-    return vars(parser.parse_args(args=argv[1:]))
 
+def check_required_config():
+    """Verify all required config options were provided via environment variables or command line arguments"""
+    if "CB_SYSTEM_KEY" not in CB_CONFIG:
+        logging.error("System Key is required, can be provided with CB_SYSTEM_KEY environment variable or --systemKey command line argument")
+        exit(-1)
+    if not "CB_SYSTEM_SECRET" in CB_CONFIG:
+        logging.error("System Secret is required, can be provided with CB_SYSTEM_SECRET environment variable or --systemSecret command line argument")
+        exit(-1)
+
+    if "deviceID" in CB_CONFIG and CB_CONFIG["deviceID"] != "" and CB_CONFIG["deviceID"] != None:
+        if "activeKey" not in CB_CONFIG:
+            logging.error("Device Active Key is required when a deviceID is specified, can be provided with the --activeKey command line argument")
+            exit(-1)
+    elif "CB_SERVICE_ACCOUNT" in CB_CONFIG and CB_CONFIG["CB_SERVICE_ACCOUNT"] != "" and CB_CONFIG["CB_SERVICE_ACCOUNT"] != None:
+        if "CB_SERVICE_ACCOUNT_TOKEN" not in CB_CONFIG:
+            logging.error("Device Service Account Token is required when a Service Account is specified, can be provided with the CB_SERVICE_ACCOUNT_TOKEN enviornment variable or --cb_service_account_token command line argument")
+            exit(-1)
+    else:
+        logging.error("Device ID/Active Key or Service Account Name and Token are required")
+        exit(-1)
+    logging.debug("Adapter Config Looks Good!")
+    
+    
 
 def setup_custom_logger(name):
     """Create a custom logger"""
@@ -233,33 +259,40 @@ if __name__ == '__main__':
     global EXIT_APP
     EXIT_APP = False
 
-    CB_CONFIG = parse_args(sys.argv)
+    parse_env_variables(os.environ)
+    parse_args(sys.argv)
+
     LOGGER = setup_custom_logger(ADAPTER_NAME)
-
-    if not CB_CONFIG['logCB']:
-        logging.debug("Setting cbLogs.DEBUG to False")
-        cbLogs.DEBUG = False
-
-    if not CB_CONFIG['logMQTT']:
-        logging.debug("Setting cbLogs.MQTT_DEBUG to False")
-        cbLogs.MQTT_DEBUG = False
+    
+    check_required_config()
 
     logging.info("Intializing ClearBlade device client")
-    logging.debug("System Key = %s", CB_CONFIG['systemKey'])
-    logging.debug("System Secret = %s", CB_CONFIG['systemSecret'])
+    logging.debug("System Key = %s", CB_CONFIG['CB_SYSTEM_KEY'])
+    logging.debug("System Secret = %s", CB_CONFIG['CB_SYSTEM_SECRET'])
     logging.debug("HTTP URL = %s", CB_CONFIG['httpURL'] + ":" + CB_CONFIG['httpPort'])
 
-    CB_SYSTEM = System(CB_CONFIG['systemKey'], CB_CONFIG['systemSecret'], CB_CONFIG['httpURL'] + \
+    CB_SYSTEM = System(CB_CONFIG['CB_SYSTEM_KEY'], CB_CONFIG['CB_SYSTEM_SECRET'], CB_CONFIG['httpURL'] + \
                        ":" + CB_CONFIG['httpPort'])
 
-    logging.info("Authenticating to ClearBlade")
-    logging.debug("Device ID = %s", CB_CONFIG['deviceID'])
-    logging.debug("Device Active Key = %s", CB_CONFIG['activeKey'])
 
-    CB_AUTH = CB_SYSTEM.Device(CB_CONFIG['deviceID'], CB_CONFIG['activeKey'])
+
+    logging.info("Authenticating to ClearBlade")
+
+    CB_AUTH = None
+
+    if "deviceID" in CB_CONFIG:
+        logging.info("Authenticating to ClearBlade with Device ID and Active Key")
+        CB_AUTH = CB_SYSTEM.Device(CB_CONFIG['deviceID'], CB_CONFIG['activeKey'])
+    elif "CB_SERVICE_ACCOUNT" in CB_CONFIG:
+        logging.info("Authenticating to ClearBlade with Device Service Account and Token")
+        CB_AUTH = CB_SYSTEM.Device(CB_CONFIG["CB_SERVICE_ACCOUNT"], authToken=CB_CONFIG["CB_SERVICE_ACCOUNT_TOKEN"])
+    else:
+        logging.error("No device info provided to authenticate to ClearBlade as")
+        exit(-1)
+
 
     #Retrieve the adapter configuration
-    if CB_CONFIG['adapterSettingsCollectionName'] != "":
+    if "adapterSettingsCollectionName" in CB_CONFIG and CB_CONFIG['adapterSettingsCollectionName'] != "":
         logging.info("Retrieving the adapter configuration settings")
         get_adapter_config()
 
